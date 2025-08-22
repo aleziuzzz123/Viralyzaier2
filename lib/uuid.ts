daebcf2b54bdcc3b13772b2b85a0ce219019c27d
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-21 19:33:55 -0500

    Update uuid.ts

--- lib/uuid.ts
+++ lib/uuid.ts
@@ -1,1 +1,11 @@
+// lib/uuid.ts
+import { v4 as uuidv4 } from 'uuid';
+
+/** Use this everywhere instead of importing 'uuid' directly */
+export default function uuid(): string {
+  return uuidv4();
+}
+
+// If you ever need the named export:
+export { uuidv4 };
 
