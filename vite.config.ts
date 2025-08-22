5ba3ec31347c0e5534e39bb2161a9e4b461c4a51
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-22 17:05:11 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// __dirname is not available in ES modules, so we define it manually.
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
