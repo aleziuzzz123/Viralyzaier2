d165bc05013d287e7ab2a600e6eda8832ab8c822
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-22 21:48:44 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     dedupe: ["@shotstack/shotstack-studio", "pixi.js", "@pixi/core", "@pixi/*"],
-    alias: {
-      "pixi.js": path.resolve(__dirname, "node_modules/pixi.js"),
-      "@pixi/core": path.resolve(__dirname, "node_modules/@pixi/core")
-    },
     preserveSymlinks: false
   },
   optimizeDeps: {
