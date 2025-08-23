f7bc5cb55c93cd102bd854059ff6ac5d09dbb20a
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-22 21:29:29 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// __dirname is not available in ES modules, so we define it manually.
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
@@ -14,5 +18,13 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["@shotstack/shotstack-studio", "pixi.js"]
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        studio: path.resolve(__dirname, 'studio.html')
+      }
+    }
   }
 });
