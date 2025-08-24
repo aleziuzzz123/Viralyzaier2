b9870cd6a3155733b26e3569d07ffa2e3b980b66
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-24 18:12:42 -0500

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -6,6 +6,7 @@
 // This registers the PixiJS audio loader globally.
 import '@pixi/sound';
 
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
@@ -13,6 +14,8 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
+    <React.StrictMode>
       <App />
+    </React.StrictMode>
   );
 }
