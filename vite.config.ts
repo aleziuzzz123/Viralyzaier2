ec5ac1f98396f57ef815393419135e49e0c7c4af
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-23 21:56:55 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
+        studio: path.resolve(__dirname, 'studio.html'),
       }
     }
   }
