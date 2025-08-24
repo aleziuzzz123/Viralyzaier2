3a6c214db715f5c0e389a8fdea8cae873b386045
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-24 18:23:13 -0500

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -2,11 +2,6 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
  */
-// MUST come before any "@shotstack/shotstack-studio" usage
-// This registers the PixiJS audio loader globally.
-import '@pixi/sound';
-
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
@@ -14,8 +9,6 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   );
 }
