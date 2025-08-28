920ccbfb5ae7beb544f9619d70552f7016ee6d97
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-28 13:50:18 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ["pixi.js"] // ensure a single Pixi instance is used everywhere
-  },
-  optimizeDeps: {
-    exclude: ["@shotstack/shotstack-studio", "pixi.js"] // don't prebundle, prevents Rollup mismatch
-  },
   build: {
     commonjsOptions: { transformMixedEsModules: true },
     sourcemap: false
